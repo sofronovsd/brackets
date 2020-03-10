9d1bb518972c6bf35711916d4e82e6ca74c59de2
sofronovsd <sofronovsd@vdcom.ru>
2020-03-10 14:12:59 +0400

    feat: realise brackets check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 === 1) return false;
+    let inputStr = String(str);
+    while (inputStr.length > 0) {
+        let isInConfig = false;
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            let config = bracketsConfig[j];
+            let param = String(config[0]) + String(config[1]);
+            let ind = inputStr.indexOf(param);
+            if (ind !== -1) {
+                inputStr = inputStr.replace(param, '');
+                isInConfig = true;
+            }
+
+        }
+        if (!isInConfig) return isInConfig;
+    }
+    return inputStr.length === 0;
 }
